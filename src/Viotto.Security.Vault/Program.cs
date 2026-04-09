1eaf56b7469451ab915b59c214203de589aa9cb9
BruhhnoV <brunoviottosantos@gmail.com>
2026-04-09 20:05:45 -0300

    feat: Implement aesmmt and KAT_AES nist test vectors

--- src/Viotto.Security.Vault/Program.cs
+++ src/Viotto.Security.Vault/Program.cs
@@ -1,11 +1,16 @@
 ﻿using System.Text;
 using Viotto.Security;
 using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 var _pbkdf2 = new Pbkdf2();
 var _pkcs7 = new Pkcs7();
 var _base64Encoder = new Base64Encoder();
 var _aesEncrypter = new Aes256Encrypter();
+var _nistRegexFilter = new Regex("^CBC(?!MCT).*?256$", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.ExplicitCapture);
+var _nistEntryRegex = new Regex(@"^COUNT = (?<count>\d+)\nKEY = (?<key>[^\n]+)\nIV = (?<iv>[^\n]+)\n(?:PLAINTEXT|CIPHERTEXT) = (?<input>[^\n]+)\n(?:PLAINTEXT|CIPHERTEXT) = (?<output>[^\n]+)$", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.ExplicitCapture);
+
 
 if (args.Length == 0)
 {
@@ -111,26 +116,110 @@ async Task DecryptAsync(string filePath)
 
 async Task TestAsync()
 {
-    byte[] key = Convert.FromHexString("0000000000000000000000000000000000000000000000000000000000000000");
-    byte[] iv = Convert.FromHexString("00000000000000000000000000000000");
-    byte[] data = Convert.FromHexString("014730f80ac625fe84f026c60bfd547d");
-    byte[] expected = Convert.FromHexString("5c9d844ed46f9885085e5d6a4f94c7d7");
+    var nistDirectory = Path.Join(Directory.GetCurrentDirectory(), "nist");
+    var files = Directory.EnumerateFiles(nistDirectory, "*.rsp", SearchOption.AllDirectories)
+        .Where(x => _nistRegexFilter.IsMatch(Path.GetFileNameWithoutExtension(x)));
 
-    var output = _aesEncrypter.Encrypt(data, key, iv);
+    foreach (var filePath in files)
+    {
+        await ProcessNistFileAsync(filePath);
+    }
+}
 
-    if (output.Length != expected.Length)
+
+async Task ProcessNistFileAsync(string filePath)
+{
+    var fileLines = (await File.ReadAllLinesAsync(filePath))
+        .Where(x => !x.StartsWith('#'))
+        .Where(x => !x.StartsWith("[ENCRYPT]"));
+
+    var fileContent = string.Join('\n', fileLines).Trim();
+    while (fileContent.IndexOf("\n\n\n") != -1)
+    {
+        fileContent = fileContent.Replace("\n\n\n", "\n\n");
+    }
+
+    var entryGroups = fileContent.Split("[DECRYPT]", StringSplitOptions.RemoveEmptyEntries);
+
+    var encryptEntries = entryGroups[0].Trim().Split("\n\n");
+    var decryptEntries = entryGroups[1].Trim().Split("\n\n");
+
+    var offset = Directory.GetCurrentDirectory().Length + 1;
+
+    var encryptLabel = $"{filePath[offset..]}.ENCRYPT";
+    foreach (var encryptEntry in encryptEntries)
+    {
+        ProcessEncryptEntry(encryptLabel, encryptEntry);
+    }
+
+    var decryptLabel = $"{filePath[offset..]}.DECRYPT";
+    foreach (var decryptEntry in decryptEntries)
+    {
+        ProcessDecryptEntry(decryptLabel, decryptEntry);
+    }
+}
+
+void ProcessEncryptEntry(string label, string textEntry)
+{
+    var entry = ParseNistEntry(textEntry);
+    Console.Write($"{label}[{entry.Count}] = ");
+
+    var output = _aesEncrypter.Encrypt(entry.Input, entry.Key, entry.IV);
+
+    if (output.Length != entry.Output.Length)
     {
         Console.WriteLine("FALHA");
         return;
     }
 
-    if (output.Zip(expected).Any(x => x.First != x.Second))
+    if (entry.Output.Zip(output).Any(x => x.First != x.Second))
     {
         Console.WriteLine("FALHA");
         return;
     }
 
     Console.WriteLine("SUCESSO");
+}
+
+void ProcessDecryptEntry(string label, string textEntry)
+{
+    var entry = ParseNistEntry(textEntry);
+    Console.Write($"{label}[{entry.Count}] = ");
+
+    var output = _aesEncrypter.Decrypt(entry.Input, entry.Key, entry.IV);
+
+    if (output.Length != entry.Output.Length)
+    {
+        Console.WriteLine("FALHA");
+        return;
+    }
+
+    if (entry.Output.Zip(output).Any(x => x.First != x.Second))
+    {
+        Console.WriteLine("FALHA");
+        return;
+    }
+
+    Console.WriteLine("SUCESSO");
+}
+
+NistEntry ParseNistEntry(string entry)
+{
+    var match = _nistEntryRegex.Match(entry);
+
+    if (!match.Success)
+    {
+        throw new UnreachableException("Falha ao processar arquivo NIST");
+    }
+
+    return new NistEntry
+    {
+        Count = uint.Parse(match.Groups["count"].Value),
+        Key = Convert.FromHexString(match.Groups["key"].Value),
+        IV = Convert.FromHexString(match.Groups["iv"].Value),
+        Input = Convert.FromHexString(match.Groups["input"].Value),
+        Output = Convert.FromHexString(match.Groups["output"].Value)
+    };
 }
 
 byte[] DerivePassword(string password, byte[] salt)
@@ -140,4 +229,13 @@ byte[] DerivePassword(string password, byte[] salt)
     var derivedKey = _pbkdf2.DeriveKey(passwordBytes, salt, 100_000, 32);
 
     return derivedKey;
+}
+
+struct NistEntry
+{
+    public uint Count { get; set; }
+    public byte[] Key { get; set; }
+    public byte[] IV { get; set; }
+    public byte[] Input { get; set; }
+    public byte[] Output { get; set; }
 }
